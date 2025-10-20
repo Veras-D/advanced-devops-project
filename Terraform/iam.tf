1cecd6e1d7ec64aca4fe21ea86c1213d27469c9d
Veras-D <dveras2310@gmail.com>
2025-10-20 01:05:36 -0300

    ci(terraform): Add new IAM ssm role for guthub workflows

--- Terraform/iam.tf
+++ Terraform/iam.tf
@@ -28,3 +28,8 @@ resource "aws_iam_instance_profile" "ec2_instance_profile" {
   name = "ec2_instance_profile"
   role = aws_iam_role.ec2_ecr_role.name
 }
+
+resource "aws_iam_role_policy_attachment" "ssm_policy_attachment" {
+  role       = aws_iam_role.ec2_ecr_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
